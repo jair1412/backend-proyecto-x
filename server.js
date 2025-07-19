c9521b91ec1e90e74d6de89d06ddb3ffdf97f043
jair1412 <tomalajair77@gmail.com>
2025-07-18 22:40:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
+// CORS - permitir origen de GitHub Pages
+app.use(cors({
+  origin: 'https://jairtc14.github.io'
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // conección a mongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
